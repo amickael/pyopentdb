95275692cb5911a1b703b2ffdc6512c496fe6b04
Andrew Mickael <andrew.mickael@tdn2k.com>
2020-07-05 20:40:43 -0500

    Added emojis to categories

--- pyopentdb/enum/Category.py
+++ pyopentdb/enum/Category.py
@@ -1,40 +1,46 @@
 from collections import namedtuple
 from enum import Enum, unique
 
-CategoryItem = namedtuple("CategoryItem", ["id", "name", "emoji"])
+CategoryItem = namedtuple("CategoryItem", ["id", "name", "emoji", "aria-label"])
 
 
 @unique
 class Category(Enum):
-    GENERAL_KNOWLEDGE = CategoryItem(9, "General Knowledge", "🧠")
-    ENTERTAINMENT_BOOKS = CategoryItem(10, "Entertainment: Books", "📚")
-    ENTERTAINMENT_FILM = CategoryItem(11, "Entertainment: Film", "🎞️")
-    ENTERTAINMENT_MUSIC = CategoryItem(12, "Entertainment: Music", "🎸")
+    GENERAL_KNOWLEDGE = CategoryItem(9, "General Knowledge", "🧠", "brain")
+    ENTERTAINMENT_BOOKS = CategoryItem(10, "Entertainment: Books", "📚", "books")
+    ENTERTAINMENT_FILM = CategoryItem(11, "Entertainment: Film", "🎞️", "film")
+    ENTERTAINMENT_MUSIC = CategoryItem(12, "Entertainment: Music", "🎸", "guitar")
     ENTERTAINMENT_MUSICALS_THEATRES = CategoryItem(
-        13, "Entertainment: Musicals & Theatres", "🎭"
+        13, "Entertainment: Musicals & Theatres", "🎭", "masks"
     )
-    ENTERTAINMENT_TELEVISION = CategoryItem(14, "Entertainment: Television", "📺")
-    ENTERTAINMENT_VIDEO_GAMES = CategoryItem(15, "Entertainment: Video Games", "🕹️")
-    ENTERTAINMENT_BOARD_GAMES = CategoryItem(16, "Entertainment: Board Games", "🎲")
-    SCIENCE_NATURE = CategoryItem(17, "Science & Nature", "🔬")
-    SCIENCE_COMPUTERS = CategoryItem(18, "Science: Computers", "💻")
-    SCIENCE_MATHEMATICS = CategoryItem(19, "Science: Mathematics", "🧮")
-    MYTHOLOGY = CategoryItem(20, "Mythology", "🔱")
-    SPORTS = CategoryItem(21, "Sports", "⚽")
-    GEOGRAPHY = CategoryItem(22, "Geography", "🗺️")
-    HISTORY = CategoryItem(23, "History", "📜")
-    POLITICS = CategoryItem(24, "Politics", "🗳️")
-    ART = CategoryItem(25, "Art", "🎨")
-    CELEBRITIES = CategoryItem(26, "Celebrities", "💃")
-    ANIMALS = CategoryItem(27, "Animals", "🐕")
-    VEHICLES = CategoryItem(28, "Vehicles", "🏎️")
-    ENTERTAINMENT_COMICS = CategoryItem(29, "Entertainment: Comics", "🦸")
-    SCIENCE_GADGETS = CategoryItem(30, "Science: Gadgets", "📱")
+    ENTERTAINMENT_TELEVISION = CategoryItem(
+        14, "Entertainment: Television", "📺", "television"
+    )
+    ENTERTAINMENT_VIDEO_GAMES = CategoryItem(
+        15, "Entertainment: Video Games", "🕹️", "joystick"
+    )
+    ENTERTAINMENT_BOARD_GAMES = CategoryItem(
+        16, "Entertainment: Board Games", "🎲", "dice"
+    )
+    SCIENCE_NATURE = CategoryItem(17, "Science & Nature", "🔬", "microscope")
+    SCIENCE_COMPUTERS = CategoryItem(18, "Science: Computers", "💻", "laptop")
+    SCIENCE_MATHEMATICS = CategoryItem(19, "Science: Mathematics", "🧮", "abacus")
+    MYTHOLOGY = CategoryItem(20, "Mythology", "🔱", "trident")
+    SPORTS = CategoryItem(21, "Sports", "⚽", "soccer ball")
+    GEOGRAPHY = CategoryItem(22, "Geography", "🗺️", "world map")
+    HISTORY = CategoryItem(23, "History", "📜", "scroll")
+    POLITICS = CategoryItem(24, "Politics", "🗳️", "ballot box")
+    ART = CategoryItem(25, "Art", "🎨", "palette")
+    CELEBRITIES = CategoryItem(26, "Celebrities", "📸", "camera")
+    ANIMALS = CategoryItem(27, "Animals", "🐕", "dog")
+    VEHICLES = CategoryItem(28, "Vehicles", "🏎️", "race car")
+    ENTERTAINMENT_COMICS = CategoryItem(29, "Entertainment: Comics", "🦸", "superhero")
+    SCIENCE_GADGETS = CategoryItem(30, "Science: Gadgets", "📱", "mobile phone")
     ENTERTAINMENT_JAPANESE_ANIME_MANGA = CategoryItem(
-        31, "Entertainment: Japanese Anime & Manga", "🇯🇵"
+        31, "Entertainment: Japanese Anime & Manga", "🇯🇵", "japanese flag"
     )
     ENTERTAINMENT_CARTOON_ANIMATIONS = CategoryItem(
-        32, "Entertainment: Cartoon & Animations", "👻"
+        32, "Entertainment: Cartoon & Animations", "🐱🐭", "cat and mouse"
     )
 
 
